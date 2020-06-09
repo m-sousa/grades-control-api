3a3c4d8a7033befdbf208952c65796fd7a2f86c2
Marcos Sousa <marcos.sousa@outlook.com>
2020-06-09 20:48:19 -0300

    Resolução atividade 01.

--- src/server.js
+++ src/server.js
@@ -1,39 +1,12 @@
 import express from "express";
-import { promises } from "fs";
-import path from "path";
-
-const app = express();
-const { readFile, writeFile } = promises;
+import routes from "./routes.js";
 
 const port = 3333;
+const app = express();
 
-app.get("/", (request, response) => {
-  return response.send("Hello world");
-});
-
-// Atividade 04
-app.get("/grades/:id", async (request, response) => {
-  const id = Number(request.params.id);
-  const gradesFile = await loadGrades();
-  const selectedGrade = gradesFile.grades.find((grade) => grade.id === id);
-
-  if (!selectedGrade) {
-    return response.status(404).send("Not found");
-  }
-
-  return response.json(selectedGrade);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
-
-async function loadGrades() {
-  try {
-    const response = await readFile(path.resolve("database/grades.json"));
-    const data = JSON.parse(response);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-}
